47d370757842f926ef86bbaccff679223b349626
SamPixel23 <samuel.cornejo2026@merlet.superate.org.sv>
2026-06-17 10:13:49 -0600

    React 1 terminada

--- practica1react/js/script.js
+++ practica1react/js/script.js
@@ -3,6 +3,7 @@ const boton = document.getElementById('btn-generar');
 const inputJuego = document.getElementById('juego');
 const inputCalificacion = document.getElementById('calificacion');
 const textoResultado = document.getElementById('resultado');
+
 // 2. ARROW FUNCTION para procesar el veredicto según la nota
 // Retorna un texto divertido usando plantillas literales (template strings)
 const obtenerVeredicto = (nota) => {
@@ -14,10 +15,10 @@ return "Decente, sirve para pasar el rato :/";
 return "Que borre la cuenta mejor x_x";
 }
 };
+
 // 3. ARROW FUNCTION principal vinculada al evento Click del botón
 boton.addEventListener('click', () => {
-// Uso de LET porque sus valores dependen de lo que el usuario escriba en el
-momento
+// Uso de LET porque sus valores dependen de lo que el usuario escriba en el momento
 let nombreJuego = inputJuego.value;
 let nota = Number(inputCalificacion.value);
 // Validación básica por si dejan los campos vacíos
@@ -27,6 +28,7 @@ return;
 }
 // Llamamos a la otra función de flecha para guardar el mensaje actitudinal
 let veredictoFinal = obtenerVeredicto(nota);
+
 // 4. Mostramos el resultado final combinando las variables
 textoResultado.innerText = `Mi veredicto sobre "${nombreJuego}" es un 
 ${nota}/10. ${veredictoFinal}`;
